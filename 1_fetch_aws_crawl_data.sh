1404ebe8fc2ff836ff486d474f04d5e68e0f51db
sagarlimbu0 <sagarl@LAPTOP-39GN690I>
2024-08-22 22:46:01 -0700

    new script sql query

--- 1_fetch_aws_crawl_data.sh
+++ 1_fetch_aws_crawl_data.sh
@@ -4,7 +4,7 @@
 
 : '
     # Arguments for source of data
-    - awk to manipulate each file line by line
+    - using awk to manipulate each file line by line
     - xargs to copy the list of files and store in {}, this placeholder will hold the filenames available and
         download from the source
     - NOTE: Check for spacing errors, permission to store files to local dir.
@@ -16,6 +16,9 @@ s3_bucket="commoncrawl"
 # dir. to files for year 2023
 source_dir="cc-index/collections.test/CC-MAIN-2023-50/indexes"
 num_files="2"
+
+# Enter the year input
+read -p "Enter the year: " year_input
 
 # files to download are saved on the text file
 aws s3 ls s3://${s3_bucket}/${source_dir}/ 2>/dev/null | head -n ${num_files} > download_filenames.txt
